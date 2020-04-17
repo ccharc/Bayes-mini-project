ec8be8318a5cbf891d3f5fbb70d52ad9d1dd2824
Charlotte Christensen <cch16@student.aau.dk>
2020-04-17 15:32:40 +0200

    update

--- elconsdatakode.R
+++ elconsdatakode.R
@@ -4,5 +4,23 @@ y = as.matrix(data[,1])
 X = as.matrix(data[,2:3])
 a=0.5
 
+
 fit1 = profile.likelihood(a,y,X)
-summary(fit1)
+
+afit=optimize(profile.likelihood,interval=c(-1,1),y=y,X=X,maximum=T)
+
+logL1 = profile.likelihood(afit$maximum,y,X,maximize=F)
+
+
+beta = coef(logL1[[2]])
+beta
+sigma = summary(logL[[2]])$sigma
+sigma
+
+mod = neg.log.profile.likelihood(theta,y,X,minimize=T)
+  
+  
+  
+  
+  
+  
